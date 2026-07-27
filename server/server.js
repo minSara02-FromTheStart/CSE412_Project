11d8a5271b1cf1060379886c77ca4a15187b1b4e
Tasnim Sara <tsn.min02@gmail.com>
2026-07-27 19:16:06 +0600

    Restore server configuration

--- server/server.js
+++ server/server.js
@@ -11,10 +11,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1);
+    console.error('MongoDB connection error:', err);
+    // process.exit(1);
   });
-
+  
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/customers', require('./routes/customerRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
